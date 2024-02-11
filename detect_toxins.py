38879bfd9a26d9883f99b38683d6c2406548c06d
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-02-11 14:21:56 -0800

    completed exericise 12.

--- detect_toxins.py
+++ detect_toxins.py
@@ -4,18 +4,30 @@
 
 def make_alert_sound():
     print('made alert sound.')
+
 def make_accept_sound():
     print('made acceptance sound')
 
-ingredients = ['sodium benzoate']
-if 'sodium nitrate' in ingredients or 'sodium benzoate' in ingredients\
-or 'sodium oxide' in ingredients:
+def has_toxin(ingredients):
+    return 'sodium nitrate' in ingredients or \
+           'sodium benzoate' in ingredients or \
+           'sodium oxide' in ingredients
+
+def alert_about_toxin():
     print('!!!')
     print('there is a toxin in the food!')    
     print('!!!')
-    make_alert_sound()
-else:
+
+def alert_toxin_free():
     print('***')
     print('Toxin Free')
     print('***')
+
+ingredients = ['sodium benzoate']
+
+if has_toxin(ingredients):
+    alert_about_toxin()
+    make_alert_sound()
+else:
+    alert_toxin_free()
     make_accept_sound()
