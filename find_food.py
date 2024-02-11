338cba1882797987fd301c8ddf56087fa37413bc
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-02-11 14:25:37 -0800

    completed exercise 15.

--- find_food.py
+++ find_food.py
@@ -2,19 +2,13 @@
 # Remove control flag
 
 def find_food(food, fridge):
-    found = False
     for item in fridge:
         if food in item:
-            found = True
-            item_name = item
-            break
-    if found:
-        return item_name
-    else:
-        return None
+            return item
+    return None
 
 if __name__ == "__main__":
-    food = 'wesabi'
-    food_list = ['onion', 'cucumber','Guacamole', 'kabob barg', 'wesabi']
+    food = 'wasabi'
+    food_list = ['onion', 'cucumber', 'Guacamole', 'kabob barg', 'wasabi']
     found_item = find_food(food, food_list)
-    print(food, "Found: " + found_item  if found_item != None else "not found")
+    print(food, "Found:", found_item if found_item is not None else "not found")
