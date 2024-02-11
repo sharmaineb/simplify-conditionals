fcdef24664f80668adb5c8d04b581c833ffc5adf
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-02-11 14:26:47 -0800

    completed exercise 16.

--- extract_position.py
+++ extract_position.py
@@ -3,17 +3,16 @@
 
 def extract_position(line):
     if not line:
-        pos = None
-    else:
-        if 'debug' in line or 'error' in line:
-            pos = None
-        else:
-            if 'x:' in line:
-                start_index = line.find('x:') + 2
-                pos = line[start_index:] # from start_index to the end.
-            else: 
-                pos = None
-    return pos
+        return None
+    
+    if 'debug' in line or 'error' in line:
+        return None
+    
+    if 'x:' not in line:
+        return None
+    
+    start_index = line.find('x:') + 2
+    return line[start_index:]  # from start_index to the end.
 
 if __name__ == "__main__":
     result1 = extract_position('|error| numerical calculations could not converge.')
