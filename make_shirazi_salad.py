9e90044d3de7b5e39dc922160ecbb2aad222cf17
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-02-11 14:24:13 -0800

    completed exercise 14.

--- make_shirazi_salad.py
+++ make_shirazi_salad.py
@@ -3,26 +3,26 @@
 
 def dice(ingredients):
     print("diced all ingredients.")
+
 def mix_all(diced_ingredients):
     print("mixed all.")
+
 def add_salt():
     print('added salt.')
+
 def pour(liquid):
-    print('poured', liquid + '.',)
+    print('poured', liquid + '.')
 
 def make_shirazi_salad(ingredients):
-    if 'cucumber' not in ingredients:
+    lacks_ingredients = 'cucumber' not in ingredients or \
+                        'tomato' not in ingredients or \
+                        'onion' not in ingredients or \
+                        'lemon juice' not in ingredients
+    
+    if lacks_ingredients:
         print('lacks ingredients.')
         return
-    if 'tomato' not in ingredients:
-        print('lacks ingredients.')
-        return
-    if 'onion' not in ingredients:
-        print('lacks ingredients.')
-        return
-    if 'lemon juice' not in ingredients:
-        print('lacks ingredients.')
-        return
+    
     dice(ingredients)
     mix_all(ingredients)
     add_salt()
@@ -31,3 +31,4 @@ def make_shirazi_salad(ingredients):
 
 if __name__ == "__main__":
     make_shirazi_salad(['cucumber', 'tomato', 'lemon juice', 'onion'])
+
