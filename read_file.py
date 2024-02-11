4b5fdbf06640e465eb1395a40abab1596b947677
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-02-11 14:31:42 -0800

    completed exercise 19.

--- read_file.py
+++ read_file.py
@@ -4,11 +4,6 @@
 # This code snippet reads up to the end of the file
 n = 16
 with open('foobar.file', 'rb') as fp:
-    running = True
-    while running:
-        chunk = fp.read(n)
-        if chunk == '': # end of file, stop running.
-            running = False
-        else:
-            print(chunk)
+    for chunk in iter(lambda: fp.read(n), b''):
+        print(chunk)
         # process(chunk)
